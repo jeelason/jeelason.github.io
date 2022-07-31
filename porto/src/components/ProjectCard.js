936755612a73e104fd4d3fa4c06fe9d04d52f05c
Jason <jlee220@gmail.com>
2022-07-30 18:49:58 -0700

    fixed favicon

--- porto/src/components/ProjectCard.js
+++ porto/src/components/ProjectCard.js
@@ -1,18 +1,18 @@
-import { Row, Col } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 
 export const ProjectCard = ({ title, description, imgUrl, gitHub, stack }) => {
   return (
     <>
       <Col size={12} sm={6} md={4}>
-        <div className="proj-imgbox">
-          <img src={imgUrl} alt="project" className="proj-img" />
-          <div className="proj-text">
-            <a href={gitHub} target="_blank" rel="noreferrer">
+        <a href={gitHub} target="_blank" rel="noreferrer">
+          <div className="proj-imgbox">
+            <img src={imgUrl} alt="project" className="proj-img" />
+            <div className="proj-text">
               <h4>{title}</h4>
-            </a>
-            <span>{description}</span>
+              <span>{description}</span>
+            </div>
           </div>
-        </div>
+        </a>
         <span>
           <p>{stack}</p>
         </span>
